3c49bc72c383c6ac65830c05a2f754d9a21237f4
Suchir Gupta <suchirgupta101@gmail.com>
2024-12-27 21:03:12 -0500

    gets progress graph working

--- workout-app/app/ProgressGraph.tsx
+++ workout-app/app/ProgressGraph.tsx
@@ -15,9 +15,13 @@ import { Exercise, getExercises } from "@/scripts/database";
 
 
 function invokeHaptic() {
-    haptics.impactAsync(haptics.ImpactFeedbackStyle.Light);
+    haptics.impactAsync(haptics.ImpactFeedbackStyle.Soft);
 };
 
+function indexToDateString(index: number, buckets: {start: Date; end: Date;}[]) {
+    console.log(index, buckets);
+    return index === - 1 ? "" : buckets[index].start.toLocaleDateString() + " - " + buckets[index].end.toLocaleDateString();
+}
 
 type DataPoint = {
     timestamp: number;
@@ -27,7 +31,9 @@ type DataPoint = {
 export default function ProgressGraph() {
     const { exerciseName } = useLocalSearchParams();
     const [ ormData, setOrmData ] = useState<DataPoint[]>([]);
+    const [ dateRangeViewing, setDateRangeViewing ] = useState<string>("");
     const [loading, setLoading] = useState(true);
+    const [buckets, setBuckets] = useState<{start: Date; end: Date;}[]>([]);
 
     useEffect(() => {
         const fetchExercises = async () => {
@@ -37,24 +43,25 @@ export default function ProgressGraph() {
             const twelveWeeksAgo = new Date(Date.now() - 12 * 7 * 24 * 60 * 60 * 1000);
 
             // TODO: fix daylight savings time issue in calcaulating buckets
-            const buckets = Array.from({ length: 12 }, (_, i) => {
+            const newBuckets = Array.from({ length: 12 }, (_, i) => {
                 const start = new Date(twelveWeeksAgo);
                 start.setDate(start.getDate() + 7 * i);
                 const end = new Date(start);
                 end.setDate(end.getDate() + 7);
                 return { start, end };
-            });
+            }); 
+            setBuckets(newBuckets);
 
             exercises.filter(exercise => exercise.name === exerciseName && exercise.created_at >=  new Date(Date.now() - 12 * 7 * 24 * 60 * 60 * 1000));
             
             let exercisesGroupedByWeek: Record<string, Exercise[]> = {};
 
-            buckets.forEach(({ start }) => {
+            newBuckets.forEach(({ start }) => {
                 exercisesGroupedByWeek[start.getTime()] = [];
             });
             exercises.forEach((currentExercise) => {
                 const { created_at: createdAt } = currentExercise;
-                const weekBucketStartTime = (buckets.find(({ start, end }) => createdAt >= start && createdAt < end) as { start: Date, end: Date }).start.getTime();
+                const weekBucketStartTime = (newBuckets.find(({ start, end }) => createdAt >= start && createdAt < end) as { start: Date, end: Date }).start.getTime();
                 exercisesGroupedByWeek[weekBucketStartTime].push(currentExercise);
             });
 
@@ -95,9 +102,13 @@ export default function ProgressGraph() {
                 <ThemedView style={{ flex: 1, justifyContent: 'center' }}>
                     <ThemedText type="title" style={styles.exerciseName}>{exerciseName}</ThemedText>
                     <ThemedText type="subtitle" style={styles.subtitle}>{"one rep max progress"}</ThemedText>
+                    <ThemedText type="subtitle" style={styles.subtitle}>{dateRangeViewing}</ThemedText>
                     <ColorfulBox childrenStyle={{backgroundColor: "#FFFFFF"}} boxStyle={{alignSelf: 'center'}} handlePress={() => {}}>                   
                         {ormData.length > 0 && (
-                            <LineChart.Provider data={ormData}>
+                            <LineChart.Provider data={ormData} onCurrentIndexChange={(index) => {
+                                invokeHaptic();
+                                setDateRangeViewing(indexToDateString(index, buckets));
+                            }}>
                             <LineChart shape={shape.curveLinear} width={Dimensions.get('window').width * 0.9} height={Dimensions.get('window').height * 0.65}>
                                 <LineChart.Path color="#31c1f5">
                                 <LineChart.Gradient />
